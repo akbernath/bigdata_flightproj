eef10bac2cd50f1b2e679d06c40d0763626afde6
ethan.edwards <edwardet@onid.oregonstate.edu>
2014-05-06 10:38:15 -0700

    first test of summary code

--- edwardetFlightCode.R
+++ edwardetFlightCode.R
@@ -28,11 +28,51 @@ tail(select(delay, -2))
 # doesn't work! try "collect"
 
 col.tst <- proc.time()
-test <- collect(select(delay, -2))
+test <- collect(delay)
 tail(test)
 proc.time() - col.tst
 
 # fortunately, gives number of unique aircraft
 # unfortunately, takes a good while! (91.68s avg, 1 execute)
+# changing (select(delay,-2)) to (delay) shaves ~33 seconds
 # additionally, gives object type VECSXP; cannot use certain
 # handy functions (length(), n_distinct())
+
+# We set a new goal here: for the largest airline volume
+# (WN), we want to create a table of mean delay time
+# (mean(arrdelay + depdelay)) from 2000-2005 by year.
+# We can then refine this to by month, depending on timing.
+
+year.time <- proc.time()
+flt.small <- filter(flights, year == 2000 ||
+                            year == 2001)
+flt.wn.small <- filter(flt.small, uniquecarrier=="WN")
+flt.yr.small <- group_by(flt.wn.small,year)
+flt.sum.small <- summarise(flt.yr.small,
+                        meandelay = mean(arrdelay+depdelay))
+sum.saved <- collect(flt.sum.small)
+arrange(sum.saved, year)s
+proc.time() - year.time
+
+# mean runtime: 223.86s (1 attempt) [OSU wifi]
+# now to check for FIVE airlines; is it a linear increase?
+# we naively expect a linear increase of 2.5x, roughly
+# if it takes roughly 600s, we're right
+# if it takes MORE, we have a problem
+# if it takes LESS, we're off to a great start
+
+year.time <- proc.time()
+flt.small <- filter(flights, year == 2000 ||
+                      year == 2001 || year == 2002 ||
+                      year == 2003 || year == 2004)
+flt.wn.small <- filter(flt.small, uniquecarrier=="WN")
+flt.yr.small <- group_by(flt.wn.small,year)
+flt.sum.small <- summarise(flt.yr.small,
+                           meandelay = mean(arrdelay+depdelay))
+sum.saved <- collect(flt.sum.small)
+arrange(sum.saved, year)
+proc.time() - year.time
+
+# mean runtime: 338.1s (1 attempt) [OSU wifi]
+# this is significantly less than a linear increase!
+# whoop whoop
