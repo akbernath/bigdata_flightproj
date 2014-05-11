1f0b55c45076a73e61191cac321b65a4b2ad9c94
ethan.edwards <edwardet@onid.oregonstate.edu>
2014-05-10 17:57:55 -0700

    commented things out, working on final output and aggregation

--- Code/edwardetFlightCode.R
+++ Code/edwardetFlightCode.R
@@ -20,80 +20,78 @@ ontime <- src_postgres("ontime",
                        password = password)
 
 flights <- tbl(ontime, "flights")
-as.tbl(head(flights))
-
-# 0c:
-
-planes <- group_by(flights, uniquecarrier)
-delay <- summarise(planes, count = n())
-tail(select(delay, -2))
-
-# doesn't work! try "collect"
-
-col.tst <- proc.time()
-test <- collect(delay)
-tail(test)
-proc.time() - col.tst
-
-# fortunately, gives number of unique aircraft
-# unfortunately, takes a good while! (91.68s avg, 1 execute)
-# changing (select(delay,-2)) to (delay) shaves ~33 seconds
-# additionally, gives object type VECSXP; cannot use certain
-# handy functions (length(), n_distinct())
-
-# We set a new goal here: for the largest airline volume
-# (WN), we want to create a table of mean delay time
-# (mean(arrdelay + depdelay)) from 2000-2005 by year.
-# We can then refine this to by month, depending on timing.
+# as.tbl(head(flights))
+# 
+# # 0c:
+# 
+# planes <- group_by(flights, uniquecarrier)
+# delay <- summarise(planes, count = n())
+# tail(select(delay, -2))
+# 
+# # doesn't work! try "collect"
+# 
+# col.tst <- proc.time()
+# test <- collect(delay)
+# tail(test)
+# proc.time() - col.tst
+# 
+# # fortunately, gives number of unique aircraft
+# # unfortunately, takes a good while! (91.68s avg, 1 execute)
+# # changing (select(delay,-2)) to (delay) shaves ~33 seconds
+# # additionally, gives object type VECSXP; cannot use certain
+# # handy functions (length(), n_distinct())
+# 
+# # We set a new goal here: for the largest airline volume
+# # (WN), we want to create a table of mean delay time
+# # (mean(arrdelay + depdelay)) from 2000-2005 by year.
+# # We can then refine this to by month, depending on timing.
+# 
+# year.time <- proc.time()
+# flt.small <- filter(flights, year == 2000 ||
+#                       year == 2001, uniquecarrier=="WN")
+# flt.yr.small <- group_by(flt.small,year)
+# flt.sum.small <- summarise(flt.yr.small,
+#                            meandelay = mean(arrdelay+depdelay))
+# sum.saved <- collect(flt.sum.small)
+# arrange(sum.saved, year)
+# proc.time() - year.time
+# 
+# # mean runtime: 226.24s (2 attempts)
+# # now to check for FIVE airlines; is it a linear increase?
+# # we naively expect a linear increase of 2.5x, roughly
+# # if it takes roughly 600s, we're right
+# # if it takes MORE, we have a problem
+# # if it takes LESS, we're off to a great start
+# 
+# year.time <- proc.time()
+# flt.small <- filter(flights, year == 2000 ||
+#                       year == 2001 || year == 2002 ||
+#                       year == 2003 || year == 2004)
+# flt.wn.small <- filter(flt.small, uniquecarrier=="WN")
+# flt.yr.small <- group_by(flt.wn.small,year)
+# flt.sum.small <- summarise(flt.yr.small,
+#                            meandelay = mean(arrdelay+depdelay))
+# sum.saved <- collect(flt.sum.small)
+# arrange(sum.saved, year)
+# proc.time() - year.time
+# 
+# # mean runtime: 338.1s (1 attempt) [OSU wifi]
+# # this is significantly less than a linear increase!
+# 
+# # now we go for the BIG one:
 
 year.time <- proc.time()
-flt.small <- filter(flights, year == 2000 ||
-                      year == 2001, uniquecarrier=="WN")
-flt.yr.small <- group_by(flt.small,year)
-flt.sum.small <- summarise(flt.yr.small,
-                           meandelay = mean(arrdelay+depdelay))
-sum.saved <- collect(flt.sum.small)
-arrange(sum.saved, year)
-proc.time() - year.time
 
-# mean runtime: 226.24s (2 attempts)
-# now to check for FIVE airlines; is it a linear increase?
-# we naively expect a linear increase of 2.5x, roughly
-# if it takes roughly 600s, we're right
-# if it takes MORE, we have a problem
-# if it takes LESS, we're off to a great start
-
-year.time <- proc.time()
-flt.small <- filter(flights, year == 2000 ||
-                      year == 2001 || year == 2002 ||
-                      year == 2003 || year == 2004)
-flt.wn.small <- filter(flt.small, uniquecarrier=="WN")
-flt.yr.small <- group_by(flt.wn.small,year)
-flt.sum.small <- summarise(flt.yr.small,
-                           meandelay = mean(arrdelay+depdelay))
-sum.saved <- collect(flt.sum.small)
-arrange(sum.saved, year)
-proc.time() - year.time
-
-# mean runtime: 338.1s (1 attempt) [OSU wifi]
-# this is significantly less than a linear increase!
-# whoop whoop
-
-# now we go for the BIG one:
-
-year.time <- proc.time()
 flt.grp <- group_by(flights,uniquecarrier,year)
 flt.grp.sum <- summarise(flt.grp,
-                         meandelay = mean(depdelay + (arrdelay - depdelay)),
-                         depdelay2 = mean(depdelay), arrdelay.adj = mean(arrdelay - depdelay))
+                         arrmean = mean(arrdelay), arrsd = sd(arrdelay))
 sum.saved <- collect(flt.grp.sum)
 sum.arr <- as.data.frame(arrange(arrange(sum.saved, year),uniquecarrier))
-sum.tab <- acast(sum.arr, uniquecarrier~year, value.var="meandelay")
-sum.tab.de <- acast(sum.arr, uniquecarrier~year, value.var="depdelay2")
-sum.tab.ar <- acast(sum.arr, uniquecarrier~year, value.var="arrdelay.adj")
-write.csv(round(sum.tab,2), "meanTotalDelaySummaryTable.csv")
-write.csv(round(sum.tab.de,2), "meanDepDelaySummaryTable.csv")
-write.csv(round(sum.tab.ar,2), "meanArrDelaySummaryTable.csv")
+pmu.out <- acast(sum.arr, year~uniquecarrier, value.var="arrmean") 
+psd.out <- acast(sum.arr, year~uniquecarrier, value.var="arrsd")
+write.csv(pmu.out, "Data/popMean.csv")
+write.csv(psd.out, "Data/popSd.csv")
+
 proc.time() - year.time
 
-# mean runtime: 249.34s (2 attempts)??!
+# mean runtime: 384.87s (1 attempt)
