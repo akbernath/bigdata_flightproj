5487cd8745d306f221610029e0945d6abfb9857e
akbernath <akbernath@yahoo.com>
2014-05-12 06:34:54 -0700

    Cleaned up histogram, final commit for bias code

--- AB_bias.R
+++ AB_bias.R
@@ -5,17 +5,9 @@
 ##   Population graphics code
 
 setwd("D:/School/Spring 14/ST599/flightproj/bigdata_flightproj")
-
-library(ggplot2)
-library(gridExtra)
-library(dplyr)
-
 df_flights <- read.csv("Data/aggregate.csv")
-df_flights[is.na(df_flights)] <- 0
 
-head(df_flights)
-
-#  Create bias matrixbias
+#  Create bias matrix
 name.vect <- c("9E", "AA", "AQ", "AS", "B6", "CO",
                "DH", "DL", "EA", "EV", "F9", "FL",
                "HA", "HP", "ML(1)", "MQ", "NW", "OH",
@@ -24,14 +16,23 @@ name.vect <- c("9E", "AA", "AQ", "AS", "B6", "CO",
 bias <- matrix(ncol=30,nrow=25)
 colnames(bias) <- name.vect
 
-
+# Loop captures bias for each year's mean delay for every carrier
 for(i in 1:30){
   bias[,i] <- df_flights[,4*i]-df_flights[,4*i-2]
 }
 
+# Mean bias for each carrier
 mean.bias <- colMeans(bias, na.rm=TRUE)
+grand.mean <- round(mean(bias, na.rm=TRUE),3)
+write.csv(mean.bias, "Data/mean_bias.csv")
 
-hist(bias, breaks=25)
 
-
-
+# Histogram of all bias
+# 2nd commit, cleaned up code and revised histogram
+pdf(paste("Bias Histogram.pdf"))
+hist(bias, breaks=23, main="Frequency Distribution of Sampling Bias
+     for Mean Arrival Delay per Year per Airline",
+     xlab=c("Sampling Bias
+            (Grand Mean = 0.335 minutes"),
+     xlim=c(-7,7), ylim=c(0,50), col="light blue")
+dev.off()
